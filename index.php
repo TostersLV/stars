ca2d533952301dc3b102e82e59689d2ee8fddaf5
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-25 13:46:23 +0200

    sigma

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
 
-echo "skibidi";
+
+
+$conn = new mysqli("localhost", "root", "", "stars");
+
+$results = $conn->query("SELECT * FROM users");
+$data = $results->fetch_assoc();
+var_dump($data);
+
+echo "<form>";
+    echo "<label> username <input name='username' />";
+    echo "<input type='password' name='password' />";
+    echo "<input type='submit' value='registreties'";
+echo "</form>";
